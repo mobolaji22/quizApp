d4107a4a8c7bb1221aec75f70e8119763c55449d
Ola Mobolaji <mobolajiola22@gmail.com>
2024-04-23 12:42:56 +0100

    update

--- question1.js
+++ question1.js
@@ -13,44 +13,91 @@ let availableQuesions = [];
 
 let questions = [
     {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
+        question: 'Who wrote the novel “1984”?',
+        choice1: 'Mark Twain',
+        choice2: 'George Orwell',
+        choice3: 'J.K. Rowling',
+        choice4: 'Charles Dickens',
+        answer: 2,
     },
     {
         question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
+            "What is the chemical symbol for Gold?",
+        choice1: "Gd",
+        choice2: "Go",
+        choice3: "Au",
+        choice4: "Ag",
         answer: 3,
     },
     {
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
+        question: "Who painted the Mona Lisa?",
+        choice1: "Vincent Van Gogh",
+        choice2: "Pablo Picasso",
+        choice3: "Leonardo da Vinci",
+        choice4: "Michelangelo",
+        answer: 3,
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        choice1: "Earth",
+        choice2: "Mars",
+        choice3: "Jupiter",
+        choice4: "Saturn",
+        answer: 3,
+    },
+    {
+        question: "What is the capital of Australia?",
+        choice1: "Sydney",
+        choice2: "Melbourne",
+        choice3: "Brisbane",
+        choice4: "Canberra",
         answer: 4,
     },
-    // {
-    //     question: "",
-    //     choice1: "",
-    //     choice2: "",
-    //     choice3: "",
-    //     choice4: "",
-    //     answer: 4,
-    // },
-
+    {
+        question: "Who discovered penicillin?",
+        choice1: "Marie Curie",
+        choice2: "Albert Einstein",
+        choice3: "Alexander Fleming",
+        choice4: "Isaac Newton",
+        answer: 3,
+    },
+    {
+        question: "What is the square root of 144?",
+        choice1: "10",
+        choice2: "13",
+        choice3: "14",
+        choice4: "12",
+        answer: 4,
+    },
+    {
+        question: "Who is the author of “Pride and Prejudice”?",
+        choice1: "Emily Bronte",
+        choice2: "Jane Austen",
+        choice3: "Charlotte Bronte",
+        choice4: "Louisa May Alcott",
+        answer: 2,
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        choice1: "K2",
+        choice2: "Kangchenjunga",
+        choice3: "Lhotse",
+        choice4: "Mount Everest",
+        answer: 4,
+    },
+    {
+        question: "Who invented the telephone?",
+        choice1: "Thomas Edison",
+        choice2: "Nikola Tesla",
+        choice3: "Alexander Graham Bell",
+        choice4: "Guglielmo Marconi",
+        answer: 3,
+    },
 ];
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
@@ -106,6 +153,7 @@ choices.forEach((choice) => {
             if (next.disabled = !choice){
                 next();
             }
+            selectedChoice.parentElement.classList.remove(classToApply);
         }, 1000);
         
     const classToApply =
@@ -114,6 +162,7 @@ choices.forEach((choice) => {
     if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
     }
+    selectedChoice.parentElement.classList.add(classToApply);
     });
 });
 incrementScore = num => {
